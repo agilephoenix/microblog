b7d380475efb8e66ca0321072e0df0a62699b722
Divyansh Kamboj <agilephoenix815.dk@gmail.com>
2018-09-03 10:27:02 +0530

    logging to the file

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler  # email error messages
-
+from logging.handlers import SMTPHandler, RotatingFileHandler  # email error messages
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)  # SET FLASK CONFIG FROM OBJECT CONFIG
@@ -32,3 +32,14 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+
+    file_handler = RotatingFileHandler(
+        'logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(messages)s [in %(pathname)s:%(lineno)d'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('Microblog startup')
